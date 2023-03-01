875385664e8b528e2860d7462b2d859b86b32dc5
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-01 10:07:38 +0300

    a

--- main.py
+++ main.py
@@ -60,16 +60,6 @@ uploaded_file = st.file_uploader(
 
 
 if uploaded_file is not None:
-    
-    model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+    image = Image.open(uploaded_file)
 
-    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-    image = Image.open(requests.get(url, stream=True).raw)
-
-    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
-
-    outputs = model(**inputs)
-    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-    probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
-    st.write(outputs)
+    st.image(image, caption='---------')
