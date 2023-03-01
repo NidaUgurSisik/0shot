c4aff6bb3251cc67492f44abcc8fc7181d9a1e03
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-01 10:20:45 +0300

    a

--- main.py
+++ main.py
@@ -63,3 +63,13 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
     st.image(image, caption='---------')
+
+    model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+
+    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+    st.write(probs)
