58fe6ea6f3ba00b96a8a8820d72eb4615b21d632
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-02 11:50:05 +0300

    a

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ uploaded_file = st.file_uploader(
     " ",
     key="1",
     help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+    accept_multiple_files=True
 )
 
 
